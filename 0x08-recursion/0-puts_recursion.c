4dc134d68063684e2eb6d77dddbe1e05d9b891ee
Gamaliel <apache664@gmail.com>
2022-09-28 08:25:11 -0700

    final update to fix error

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Prints a string, followed by a new line.
- * @s: The string to be printed.
+ * _puts_recursion - this rints a string, then a new line.
+ * @s: -string to be printed.
  */
 void _puts_recursion(char *s)
 {
