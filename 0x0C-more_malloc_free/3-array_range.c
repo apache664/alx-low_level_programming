6e9b035de1fcf3751fabafaecbaee0f86a96752f
Gamaliel <apache664@gmail.com>
2022-10-06 15:46:50 -0700

    fix errors in conditional statement

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,28 +1,31 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * array_range - this creates an array of integers, i.e min to max
- * @min: d min number to start
- * @max: d max number to start
- * Return: pointer
- */
+ * array_range - this creates an array of integers
+ *
+ * @min: d min number of elements
+ * @max: d max number of elements
+ *
+ * Return: array pointer address
+ *         NULL if it fails
+*/
 
 int *array_range(int min, int max)
 {
-  int *ptr;
-  int diff, i;
+	int i, *array;
 
-  if (min > max)
-    return (NULL);
-  diff = max - min;
-  ptr = malloc((diff + 1) * sizeof(int));
-  if (ptr == NULL)
-    return (NULL);
-  for (i = 0; i <= diff; min++, i++)
-    {
-      ptr[i] = min;
-    }
-  return (ptr);
+	if (min > max)
+		return (NULL);
+
+	array = malloc((max - min + 1) * sizeof(int));
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; min <= max; i++)
+	{
+		array[i] = min;
+		min++;
+	}
+	return (array);
 }
