4b951b3a135d2c3f8fef3a5612df6c6e5f7e69aa
Gamaliel <apache664@gmail.com>
2022-10-06 15:55:27 -0700

    fix code indents 2,4,6

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,40 +3,51 @@
 #include <stdio.h>
 
 /**
- *_calloc - this allocate the memory for an array called usingmalloc
- * @nmemb: the number elements
- * @size: the size of bytes
- * Return: pointer
- */
-
-void *_calloc(unsigned int nmemb, unsigned int size)
-{
-  void *ptr;
-
-  if (nmemb == 0 || size == 0)
-    return (NULL);
-  ptr = malloc(nmemb * size);
-  if (ptr == NULL)
-    return (NULL);
-  _memset(ptr, 0, (nmemb * size));
-  return (ptr);
-}
-
-/**
- *_memset - this sets first n bytes of the memory allocation area
- * @s: an array to set
- * @b: set it to
- * @n: the first n bytes
- * Return: s
- */
+ * _memset - this fills the memory with a constant byte
+ *
+ * @s: input pointer that represents memory
+ *     block to fill
+ * @b: characters to fill
+ * @n: number of bytes to be filled
+ *
+ * Return: A pointer to the filled memory area
+*/
 
 char *_memset(char *s, char b, unsigned int n)
 {
-  unsigned int i;
+	unsigned int i;
 
-  for (i = 0; i < n; i++)
-    {
-      s[i] = b;
-    }
-  return (s);
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
+
+/**
+ * _calloc - a function that allocates
+ *           memory for an array using malloc
+ *
+ *           It is basically the equivalent to
+ *           malloc followed by memset
+ *
+ * @nmemb: size of array
+ * @size: size of each element
+ *
+ * Return: pointer with new allocated memory
+ *         or NULL if it fails
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+
+	_memset(p, 0, nmemb * size);
+
+	return (p);
 }
