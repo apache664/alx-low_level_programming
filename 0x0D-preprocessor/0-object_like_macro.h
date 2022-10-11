8bed4ef9bf63eeae46153705e5e52c1e4ececf80
Gamaliel <apache664@gmail.com>
2022-10-10 19:14:53 -0700

    second fix

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,12 +1,3 @@
-#ifndef OBJECT_LIKE_MACRO_H
-#define OBJECT_LIKE_MACRO_H
-
-/*
- * File: 0-object_like_macro.h
- * Desc: a header file that defines a macro named
- *       SIZE as an ancroynm for the token 1024.
- */
-
+#ifndef SIZE
 #define SIZE 1024
-
 #endif
